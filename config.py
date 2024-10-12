e46f2024f0063bb6a3eed7573df61541193e4554
WangLinXMusic <pyaesone5official@gmail.com>
2024-10-12 11:04:52 +0630

    Update config.py

--- config.py
+++ config.py
@@ -37,7 +37,7 @@ HEROKU_API_KEY = getenv("HEROKU_API_KEY")
 
 UPSTREAM_REPO = getenv(
     "UPSTREAM_REPO",
-    "https://github.com/NhoetKyaiteKaungLayy/FlashXMusic",
+    "https://github.com/WangLinXMusic/FlashXMusic",
 )
 UPSTREAM_BRANCH = getenv("UPSTREAM_BRANCH", "main")
 GIT_TOKEN = getenv(
